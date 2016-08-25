5e5763b9dd4c7ed1147602208626c2472d084658
Nhala <lidiga03@ucm.es>
2016-08-25 13:02:09 +0200

    Intento de hacer el boton destruir, visualmente esta, pero parece no funcionar

--- ExamenTPJunio/Practica6/src/es/ucm/fdi/tp/practica5/view/DestruirMove.java
+++ ExamenTPJunio/Practica6/src/es/ucm/fdi/tp/practica5/view/DestruirMove.java
@@ -2,6 +2,7 @@ package es.ucm.fdi.tp.practica5.view;
 
 import java.util.List;
 
+import es.ucm.fdi.tp.basecode.bgame.Utils;
 import es.ucm.fdi.tp.basecode.bgame.model.Board;
 import es.ucm.fdi.tp.basecode.bgame.model.GameMove;
 import es.ucm.fdi.tp.basecode.bgame.model.Piece;
@@ -26,20 +27,70 @@ public class DestruirMove extends GameMove{
 
 	@Override
 	public void execute(Board board, List<Piece> pieces) {
-		// TODO Auto-generated method stub
+		int rows = board.getRows();
+		int cols = board.getCols();
+		int randomRows = Utils.randomInt(rows);
+		int randomCols = Utils.randomInt(cols);
+		int initRow = randomRows;
+		int initCol = randomCols;
+		Piece p = getPiece();
+		do{
+			if(p.equals(board.getPosition(initRow, initCol))){
+				Destruir(board, pieces, initRow, initCol, radio, p);
+				return;
+			}
+			initCol = (initCol + 1)%cols;
+			if(initCol == 0){
+				initRow = (initRow + 1)%rows;
+			}
+		}while(initRow != randomRows || initCol != randomCols);
 		
+	}
+
+	private void Destruir(Board board, List<Piece> pieces, int initRow, int initCol, Integer radio, Piece p) {
+		if(p.equals(board.getPosition(initRow, initCol))){
+			for(int i = initRow; i < 6; i++){
+				for(int j = initCol; j < 6; j++){
+					if(p.equals(board.getPosition(i, j)) && enTablero(board, initRow, initCol) && noObstaculo(board, pieces, initRow, initCol, radio)){
+						board.setPosition(i, j, null);
+						board.setPieceCount(p, pieces.size() - 1);
+					}
+				}
+			}
+		}
+		
+	}
+	
+	private boolean enTablero(Board board, int rows, int cols){
+		return (rows >= 0 && cols >= 0 && rows < board.getRows() && cols < board.getCols());
+	}
+	
+	private boolean noObstaculo(Board board, List<Piece>pieces, int row, int col, int radio){
+		return (enTablero(board, row, col) && (board.getPosition(row, col) == null || pieces.contains(board.getPosition(row, col))));
 	}
 
 	@Override
 	public GameMove fromString(Piece p, String str) {
-		// TODO Auto-generated method stub
-		return null;
+		String[] palabras = str.split(" ");
+		if(palabras.length != 2 || !palabras[0].equalsIgnoreCase("full")){
+			return null;
+		}
+		try{
+			int radio = Integer.parseInt(palabras[1]);
+			return new DestruirMove(p, radio);
+		}catch(NumberFormatException e){
+			return null;
+		}
 	}
 
 	@Override
 	public String help() {
-		// TODO Auto-generated method stub
-		return null;
+		return "full radio";
+	}
+	
+	@Override
+	public String toString() {
+		return "Destruccion en la posicion de " + getPiece();
 	}
 
 }
